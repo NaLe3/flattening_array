b974576db271deefa0d67050fc3c172af8d697b0
Nathan Lebas <nathanlebas3@gmail.com>
2022-07-06 16:56:26 +0200

    create test file with user case scenario

---
+++ flattening_array_test.rb
@@ -0,0 +1,35 @@
+require "minitest/autorun" #load minitest v 5.16.2
+require_relative "flattening_array.rb"
+
+
+class FlatteningArrayTest < Minitest::Test
+
+  #Testing when the input is not an array
+  def test_with_no_array
+    
+  end
+
+  #Testing a simple array with a 1 dimensional array
+  def test_with_1_dimensional_array
+    
+  end
+  
+  #Testing a simple array with a 2 dimensional array
+  def test_with_2_dimensional_array
+    
+  end
+
+  #Testing an array with 3 dimensional array
+  def test_with_2_dimensional_array
+    
+  end
+
+  def test_array_including_a_string
+    
+  end
+
+  def test_array_including_a_hash
+    
+  end
+
+end
